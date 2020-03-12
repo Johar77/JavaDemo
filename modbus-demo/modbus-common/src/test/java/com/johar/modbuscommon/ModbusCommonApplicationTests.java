77d1eb03b11e140585c208163017bea4f6ae9618
Johar77 <Johar77@163.com>
2020-03-13 00:21:51 +0800

    提交modbus-demo

---
+++ modbus-demo/modbus-common/src/test/java/com/johar/modbuscommon/ModbusCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johar.modbuscommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModbusCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ modbus-demo/modbus-core/src/main/java/com/johar/modbuscore/ModbusCoreApplication.java
@@ -0,0 +1,13 @@
+package com.johar.modbuscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModbusCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModbusCoreApplication.class, args);
+    }
+
+}
